8774cc6e82f19ecab8d71a9d6f7c29216885fa39
arojitpronoy45 <arojitpronoyqa@gmail.com>
2023-11-04 23:33:15 -0400

    selenium

---
+++ src/JavaHomework/HW_SL01.java
@@ -0,0 +1,46 @@
+package JavaHomework;
+
+public class HW_SL01 {
+	
+	
+	//	A)tag with a single attribute below: 
+	//  source used https://www.amazon.com/
+	// 	input[@id='twotabsearchtextbox']
+	 
+	
+	// 	b)Tag with multiple attributes with 'and' logic:
+	//  source used	https://portal.cms.gov/portal/
+	//	input[@id='cms-login-userId' and @name='user-d']
+	  
+	
+	
+	//	c)Tag with inner text:
+	//  source used https://www.amazon.com/
+	//	a[text()='Holiday Deals']
+	
+	
+	// 	d)Tag with partial inner text from any website:
+	//  source used https://www.amazon.com/
+	//	a[contains(text(), 'Pet')]
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
